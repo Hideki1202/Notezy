e0718f38cdf405a368324be306ffc81f989cbc23
Hideki1202 <matheus.ueno@germinare.org.br>
2025-05-27 12:35:41 -0300

    fix: cors

--- notezy-api/app/__init__.py
+++ notezy-api/app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 
 mongo = PyMongo()
 
 def create_app():
     app = Flask(__name__)
     app.config["MONGO_URI"] = "mongodb://localhost:27017/meu_banco"
+    CORS(app)
 
     mongo.init_app(app)
 
